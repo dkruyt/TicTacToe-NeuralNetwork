e712d7584f92be5a93273ab8dee9f111734988a8
Dennis Kruyt <dennis@kruyt.org>
2024-01-24 21:49:36 +0100

    Update model creation functions in model.py

--- modules/model.py
+++ modules/model.py
@@ -12,7 +12,31 @@ def create_mlp_model(input_shape, dense_units, dropout_rate):
         layers.Dropout(dropout_rate, name='dropout_2'),
         layers.Dense(9, activation='linear', name='output_layer')  # 9 output nodes for Tic-Tac-Toe
     ], name='MLP_Model')
+    model.compile(optimizer='adam', loss='mean_squared_error')
     return model
+
+def create_policy_mlp_model(input_shape, dense_units, dropout_rate):
+    model = keras.Sequential([
+        layers.Dense(128, activation='relu', input_shape=input_shape, name='input_layer'),
+        layers.Dropout(dropout_rate, name='dropout_1'),
+        layers.Dense(dense_units, activation='relu', name='hidden_layer_1'),
+        layers.Dropout(dropout_rate, name='dropout_2'),
+        layers.Dense(9, activation='softmax', name='output_layer')  # softmax for multi-class classification
+    ], name='Policy_MLP_Model')
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+def create_value_mlp_model(input_shape, dense_units, dropout_rate):
+    model = keras.Sequential([
+        layers.Dense(dense_units, activation='relu', input_shape=input_shape, name='input_layer'),
+        layers.Dropout(dropout_rate, name='dropout_1'),
+        layers.Dense(dense_units, activation='relu', name='hidden_layer_1'),
+        layers.Dropout(dropout_rate, name='dropout_2'),
+        layers.Dense(1, activation='tanh', name='output_layer')  # tanh for regression (value-based)
+    ], name='Value_MLP_Model')
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    return model
+
 
 # CNN Model
 def create_cnn_model(input_shape, dense_units, dropout_rate):
@@ -24,6 +48,7 @@ def create_cnn_model(input_shape, dense_units, dropout_rate):
         layers.Dropout(dropout_rate, name='dropout'),
         layers.Dense(9, activation='linear', name='output_layer')
     ], name='CNN_Model')
+    model.compile(optimizer='adam', loss='mean_squared_error')
     return model
 
 # RNN Model
@@ -35,4 +60,5 @@ def create_rnn_model(input_shape, dense_units, dropout_rate):
         layers.Dropout(dropout_rate, name='dropout'),
         layers.Dense(9, activation='linear', name='output_layer')
     ], name='RNN_Model')
+    model.compile(optimizer='adam', loss='mean_squared_error')
     return model
